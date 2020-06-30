05a9f8d3c2b1bdc378a75ea63bb308edeaae6e50
Francisco <ainzdono@gmail.com>
2020-06-30 19:27:38 -0400

    PruebaSolemne2
    No cache nada hice lo q pude

---
+++ PruebaPizza/Menus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaPizza
+{
+    public partial class Menus : Form
+    {
+        public Menus()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            LinqCajero _tablaCajeros = new LinqCajero();
+            var cajeros = from cajero in _tablaCajeros.Cajero select cajero;
+            _gvCajeros.DataSource = cajeros;
+        }
+    }
+}
